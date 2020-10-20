f9bc0aacc308cc6cebfcb1cb774bb8dc303d142a
OsOs11 <mo0197645@gmail.com>
2020-10-20 12:40:43 +0200

    import new module and write some code

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 import os
 import time
+import datetime
+var=datetime.datetime.now()
 i=input("Say my name? " )
 if i == "Heisenberg":
-    print("You're Goddamn right!")
+    print("You're Goddamn right!" + "\n" + "Time now is: " + str(var))
 else:
     print("Wrong b****")
+    
     
